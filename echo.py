a3e1ced360c51ac17f44b29e75ff49197e285290
Kaloong <kaloong@B13.local>
2020-02-03 22:12:38 +0000

    Trying out echo server with IService and IPlugins

--- echo.py
+++ echo.py
@@ -3,6 +3,8 @@ from twisted.internet import protocol, reactor
 class Echo(protocol.Protocol):
     def dataReceived(self, data):
         self.transport.write(data)
+    def connectionLost(self, reason):
+        self.transport.loseConnection()
 
 class EchoFactory(protocol.Factory):
     def buildProtocol(self, addr):
